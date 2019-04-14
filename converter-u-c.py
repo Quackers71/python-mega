25712a07f1904262daf9fce5c21b43f3c4db1a3e
quackers71 <24840026+Quackers71@users.noreply.github.com>
2019-04-14 17:26:09 +0100

    added explicit parameter to converter-u-c.py

--- converter-u-c.py
+++ converter-u-c.py
@@ -1,6 +1,6 @@
 
-def converter(original_unit, coefficient):
+def converter(original_unit, coefficient=0.3048):
     return original_unit * coefficient
 
 
-print(converter(10, 0.3048))
+print(converter(10, 0.62))
