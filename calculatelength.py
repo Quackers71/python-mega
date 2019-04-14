92cc06471562d778afae77bcc307d40c8b9a6918
quackers71 <24840026+Quackers71@users.noreply.github.com>
2019-04-14 09:25:48 +0100

    CalculateLength function

---
+++ calculatelength.py
@@ -0,0 +1,14 @@
+
+def length_of_string():
+    user_input = input("Enter some Characters : ")
+    return len(user_input)
+
+
+print(length_of_string())
+
+
+def string_length(mystring):
+    return len(mystring)
+
+
+print(string_length("Hello World"))
