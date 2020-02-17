af7d087fe780af49c5800025b5ff373de3460bed
Quacks <24840026+Quackers71@users.noreply.github.com>
2020-02-17 19:12:18 +0000

    Started Section3: The Basics: Data Types

---
+++ Section3/simpleTypes.py
@@ -0,0 +1,22 @@
+# declare some variables
+mynum = 10
+mytext = "Hello!\r"
+
+# print them out
+print(mynum, mytext)
+
+# declare simpleTypes variables
+x = 10
+y = "10"
+
+# print x out, type of x, total adding x to x, then print total
+print("\nx = ",x)
+print("x is a type of ",type(x))
+sum1 = x + x
+print("x + x = ",sum1)
+
+# print y out, type of y, total adding y to y, then print total
+print("y = \"" + y + "\"")
+print("y is a type if ",type(y))
+sum2 = y + y
+print("y + y = ",sum2)
