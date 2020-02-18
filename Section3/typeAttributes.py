2a24624b29cd755af349535287bd7753263c5759
Quacks <24840026+Quackers71@users.noreply.github.com>
2020-02-18 21:37:45 +0000

    Type Attributes & Did You Know?

---
+++ Section3/typeAttributes.py
@@ -0,0 +1,23 @@
+student_grades = [9.1, 8.8, 7.5]
+print("Student #1 scored:",student_grades[0])
+print("\r")
+
+print(dir(list))
+print("\r")
+print(dir(int))
+print("\"r")
+print(dir(float))
+print("\r")
+print(dir(str))
+print("\r")
+
+help(str.upper)
+print("\r")
+
+help(str.title)
+
+print("hello".upper())
+print("\r")
+
+myword = "hello world!"
+print(myword.title())
