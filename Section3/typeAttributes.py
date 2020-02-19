597aeec57af278dce24cc9fddb099c57b4377b6f
Quacks <24840026+Quackers71@users.noreply.github.com>
2020-02-19 20:46:19 +0000

    updated typeAttr & Output

--- Section3/typeAttributes.py
+++ Section3/typeAttributes.py
@@ -5,21 +5,28 @@ student_grades = [9.1, 8.8, 7.5]
 print("Student #1 scored:",student_grades[0])
 print("\r")
 
+print("dir(list)")
 print(dir(list))
 cr()
+print("dir(int)")
 print(dir(int))
 cr()
+print("dir(float)")
 print(dir(float))
 cr()
+print("dir(str)")
 print(dir(str))
 cr()
 
+print("help(str.upper)")
 help(str.upper)
 cr()
 
+print("help(str.title)")
 help(str.title)
 cr()
 
+print("\"hello\".upper()")
 print("hello".upper())
 cr()
 
