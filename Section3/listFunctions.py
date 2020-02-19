c03adbb1113650d1aaa40a05a82e8891c2ea3670
Quacks <24840026+Quackers71@users.noreply.github.com>
2020-02-19 21:24:50 +0000

    listTypes2 & built-in functions list

---
+++ Section3/listFunctions.py
@@ -0,0 +1,7 @@
+print("print(1) is a built-in function")
+
+print("'hello'.upper() is a method") # .upper() is a method
+
+print("To view a full list of built-in functions use 'dir(__builtins__)'")
+
+print(dir(__builtins__))
---
+++ Section3/listTypes2.py
@@ -0,0 +1,11 @@
+student_grades = [9.1, 8.8, 7.5]
+print("Student #1 scored:",student_grades[0])
+
+mysum = sum(student_grades)
+print("sum(student_grades) : ",mysum)
+
+length = len(student_grades)
+print("len(student_grades) : ", length)
+
+mean = mysum / length
+print("The mean of the student_grades : ", mean)
