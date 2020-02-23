dfa84ab3eaa32c68c43294946df065e2d551cdf7
Quacks <24840026+Quackers71@users.noreply.github.com>
2020-02-23 23:25:34 +0000

    If Conditional Example

--- Section5/mean.py
+++ Section5/mean.py
@@ -1,7 +1,11 @@
-def mean(mylist):
-    print("Function started!")
-    the_mean = sum(mylist) / len(mylist)
+def mean(value):
+    if type(value)  == dict:
+        the_mean = sum(value.values()) / len(value)    
+    else: 
+        the_mean = sum(value) / len(value)
+    
     return the_mean
 
+monday_temperatures = [8.8, 9.1, 9.9]
 student_grades = {"Kylie": 9.1, "Eva": 8.8, "Tarquin": 7.5}
-print(mean(student_grades))
+print(mean(monday_temperatures))
