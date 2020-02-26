0e7e3591c5985c0bd396f00a8c9b88694cd49f9a
Quackers <robq71@gmail.com>
2020-02-26 08:06:54 +0000

    updated to raw_input

--- Section6/userInput2.py
+++ Section6/userInput2.py
@@ -1,6 +1,6 @@
 # Two methods of string formatting
 
-user_input = input("Enter your name: ")
+user_input = raw_input("Enter your name: ")
 message = "Hello %s!" % user_input
 #message = f"Hello {user_input}"
 print(message)
