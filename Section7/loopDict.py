46ed6d6729fd4d09010c0626f5c557250b8a1d83
Quacks <24840026+Quackers71@users.noreply.github.com>
2020-02-27 21:46:09 +0000

    Dictionary for loops and Formatting

---
+++ Section7/loopDict.py
@@ -0,0 +1,11 @@
+student_grades = {"Kylie": 9.1, "Eva": 8.8, "Tarquin": 7.5}
+
+for grades in student_grades.items():
+    print(grades)
+
+print(type(grades))
+
+for grades in student_grades.values():
+    print(grades)
+
+print(type(grades))
---
+++ Section7/loopDictFormatEx34.py
@@ -0,0 +1,5 @@
+phone_numbers = {"John Smith": "+37682929928", "Marry Simpons": "+423998200919"}
+
+for details in phone_numbers.items():
+    print("%s: %s" % (details[0], details[1]))
+    #print("{}: {}").format(details[0], details[1])
