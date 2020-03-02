afce912881d073e124dd1345d8c1a305e5bd17c6
Quacks <24840026+Quackers71@users.noreply.github.com>
2020-03-02 19:25:21 +0000

    up to Ex42: Indefinite Number of Strings

---
+++ Section10/funcManyArgs.py
@@ -0,0 +1,6 @@
+def mean(*args):
+    return sum(args) / len(args)
+
+print(mean(1, 3, 4))
+
+# A Function with an Arbitrary Number of Non-keyword Arguments
---
+++ Section10/funcStringsArgs.py
@@ -0,0 +1,6 @@
+def foo(*args):
+    
+    
+    return 
+
+print(foo("snow", "glacier", "iceberg"))
