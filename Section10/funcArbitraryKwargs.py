3a12faa6f3ed39ace7f924b0dc4acc78eafc69da
Quacks <24840026+Quackers71@users.noreply.github.com>
2020-03-02 21:07:21 +0000

    Completed Section 10: More on Functions

---
+++ Section10/funcArbitraryKwargs.py
@@ -0,0 +1,7 @@
+def mean(**kwargs):
+    return kwargs
+
+print(mean(a=1, b=2, c=3))
+
+'''Output: 
+{'a': 1, 'b': 2, 'c': 3}'''
---
+++ Section10/kwargsEx43.py
@@ -0,0 +1,4 @@
+def find_sum(**kwargs):
+    return sum(kwargs.values())
+    
+print(find_sum(a=4, b=5))
