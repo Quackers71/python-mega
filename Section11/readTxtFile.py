826b89240402e2196af0585b06c37a95bfa8fc41
Quacks <24840026+Quackers71@users.noreply.github.com>
2020-03-05 21:19:27 +0000

    Using different filepaths

--- Section11/readTxtFile.py
+++ Section11/readTxtFile.py
@@ -1,9 +1,4 @@
-""" myfile = open("fruits.txt")
-content = myfile.read()
-myfile.close() """
-
-with open("fruits.txt") as myfile:
+with open("files/fruits.txt") as myfile:
     content = myfile.read()
 
 print(content)
-
