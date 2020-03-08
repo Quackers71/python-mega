028e124394e21d04146ede3856dde676e4857690
Quacks <24840026+Quackers71@users.noreply.github.com>
2020-03-08 22:05:09 +0000

    Completed Section 12: Imported Modules

---
+++ Section12/third-PartyModules.py
@@ -0,0 +1,10 @@
+import time, os, pandas
+
+while True:
+    if os.path.exists("files/temps_today.csv"):
+        data = pandas.read_csv("files/temps_today.csv")
+        print(data.mean()["st1"])
+    else:
+        print("The File does not exist.")
+    
+    time.sleep(10)
