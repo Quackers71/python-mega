00ef5fdd53e5abddd4cf2934c365b0aa34410e79
Quacks <24840026+Quackers71@users.noreply.github.com>
2020-03-10 17:13:52 +0000

    updated app1.py with comments and output of 'dog'

--- Section13/app1.py
+++ Section13/app1.py
@@ -1,11 +1,17 @@
+# use the json modules
 import json
 
+# Creating the File Object using the open("File_Path") method
 data = json.load(open("data.json"))
 
+''' create the translate function and return 'data[w]'
+    'w' is a Local variable '''
 def translate(w):
     return data[w]
 
+#use the 'word' Global variable for the input function
 word = input("Enter a word: ")
 
+# printout using the translate function of the Global varaible 'word' input
 print(translate(word))
 
