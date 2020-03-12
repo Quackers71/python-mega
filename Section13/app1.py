c92c1fd03f84d5d8f44e1e24dc30311e54ba1a13
Quacks <24840026+Quackers71@users.noreply.github.com>
2020-03-12 21:48:06 +0000

    93. Recommending the Best Match

--- Section13/app1.py
+++ Section13/app1.py
@@ -1,5 +1,6 @@
 # use the json modules
 import json
+from difflib import get_close_matches
 
 # Creating the File Object using the open("File_Path") method
 data = json.load(open("data.json"))
@@ -10,6 +11,8 @@ def translate(w):
     w = w.lower()
     if w in data:
         return data[w]
+    elif len(get_close_matches(w, data.keys(), cutoff=0.8)) > 0:
+        return "Did you mean %s instead?" % get_close_matches(w, data.keys())[0]
     else:
         return "The word doesn't exist!  Please double check it..."
 
@@ -18,4 +21,3 @@ word = input("Enter a word: ")
 
 # printout using the translate function of the Global varaible 'word' input
 print(translate(word))
-
