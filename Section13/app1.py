931e581752b8307721f6e2d9aee0050d3315eead
Quacks <24840026+Quackers71@users.noreply.github.com>
2020-03-12 22:23:18 +0000

    94. Confirmation from the User

--- Section13/app1.py
+++ Section13/app1.py
@@ -1,4 +1,4 @@
-# use the json modules
+# use the json modules and from difflib import g_c_m
 import json
 from difflib import get_close_matches
 
@@ -11,9 +11,21 @@ def translate(w):
     w = w.lower()
     if w in data:
         return data[w]
-    elif len(get_close_matches(w, data.keys(), cutoff=0.8)) > 0:
-        return "Did you mean %s instead?" % get_close_matches(w, data.keys())[0]
+    # Check the length of the close match and is greater than 0
+    elif len(get_close_matches(w, data.keys())) > 0:
+        # Ask User for close word
+        yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(w, data.keys())[0])
+        if yn.capitalize() == "Y":
+            # if Y(yes) return the word
+            return data[get_close_matches(w, data.keys())[0]]
+        elif yn.capitalize() == "N":
+            # else if N(no) return don't exist!
+            return "The word doesn't exist!  Please double check it..."
+        else:
+            # else You crap!
+            return "We didn't understand your entry."
     else:
+        # No matches!
         return "The word doesn't exist!  Please double check it..."
 
 #use the 'word' Global variable for the input function
