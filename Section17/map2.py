c45f82eb1c1b10ec1825a5a184ddfc1863eee756
Quacks <24840026+Quackers71@users.noreply.github.com>
2020-05-12 19:05:32 +0100

    using the FeatureGroup Object so you add multiple features

--- Section17/map2.py
+++ Section17/map2.py
@@ -1,6 +1,8 @@
 import folium
 map = folium.Map(location=[53.977048, -1.559376], zoom_start=14, tiles = "Stamen Terrain")
 
-map.add_child(folium.Marker(location=[53.977048, -1.559376], popup="Hi, I am a Marker", icon=folium.Icon('green')))
+fg = folium.FeatureGroup(name="My Map")
+fg.add_child(folium.Marker(location=[53.977048, -1.559376], popup="40 Beckwith Rd", icon=folium.Icon('green')))
+map.add_child(fg)
 
 map.save("Map2.html")
