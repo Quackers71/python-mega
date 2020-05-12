1025cc399e53e0fbf3dd9686529154541ad0149c
Quacks <24840026+Quackers71@users.noreply.github.com>
2020-05-12 21:43:17 +0100

    added ELEVation in Metres to the Markers

--- Section17/map3.py
+++ Section17/map3.py
@@ -4,13 +4,14 @@ import pandas
 data = pandas.read_csv("Volcanoes.txt")
 lat = list(data["LAT"])
 lon = list(data["LON"])
+elev = list(data["ELEV"])
 
 map = folium.Map(location=[37.7749, -122.4194], zoom_start=6, tiles = "Stamen Terrain") # San Francisco
 
 fg = folium.FeatureGroup(name="My Map")
 
-for lt, ln in zip(lat, lon):
-    fg.add_child(folium.Marker(location=[lt, ln], popup="My Markers", icon=folium.Icon('green')))
+for lt, ln, el in zip(lat, lon, elev):
+    fg.add_child(folium.Marker(location=[lt, ln], popup=str(el)+"m", icon=folium.Icon(color='green')))
 
 map.add_child(fg)
 
