7743bc60cbaad8c9bbe6c5a735e5eeb7f584372a
Quacks <24840026+Quackers71@users.noreply.github.com>
2020-05-15 20:56:56 +0100

    variable not defined

--- Section18/errors2.py
+++ Section18/errors2.py
@@ -1,4 +1,15 @@
 a = 1
 b = "2"
+
 print(int(2.5))
 print(str(a) + b)
+print(c)
+
+## Output
+# C:\Users\Rob\Desktop\AWS EC2\python-mega\Section18>py errors2.py
+# 2
+# 12
+# Traceback (most recent call last):
+#   File "errors2.py", line 5, in <module>
+#     print(c)
+# NameError: name 'c' is not defined
