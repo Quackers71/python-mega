bb68b68e8801ac4548ee3796e70d6cd2e661aa32
Quacks <24840026+Quackers71@users.noreply.github.com>
2020-05-15 20:53:49 +0100

    Corrected to SyntaxError

--- Section18/errors2.py
+++ Section18/errors2.py
@@ -1,13 +1,18 @@
 a = 1
 b = "2"
-print(int(2.5))
+print(int(2.5)
 print(a + b)
 
-# output
-
+## Output
 # C:\Users\Rob\Desktop\AWS EC2\python-mega\Section18>py errors2.py
 # 2
 # Traceback (most recent call last):
 #   File "errors2.py", line 4, in <module>
 #     print(a + b)
 # TypeError: unsupported operand type(s) for +: 'int' and 'str'
+
+# C:\Users\Rob\Desktop\AWS EC2\python-mega\Section18>py errors2.py
+#   File "errors2.py", line 4
+#     print(a + b)
+#     ^
+# SyntaxError: invalid syntax
