c62adcc2be5961eddb9879da7d0e63d2391dacf2
Quacks <24840026+Quackers71@users.noreply.github.com>
2020-05-15 20:51:54 +0100

    143. Runtime Errors

---
+++ Section18/errors2.py
@@ -0,0 +1,13 @@
+a = 1
+b = "2"
+print(int(2.5))
+print(a + b)
+
+# output
+
+# C:\Users\Rob\Desktop\AWS EC2\python-mega\Section18>py errors2.py
+# 2
+# Traceback (most recent call last):
+#   File "errors2.py", line 4, in <module>
+#     print(a + b)
+# TypeError: unsupported operand type(s) for +: 'int' and 'str'
