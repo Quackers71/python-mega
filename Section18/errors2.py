2190c885948ca648c9d761e5f2af7222c76ef0f5
Quacks <24840026+Quackers71@users.noreply.github.com>
2020-05-15 20:59:32 +0100

    All fixed

--- Section18/errors2.py
+++ Section18/errors2.py
@@ -1,15 +1,8 @@
 a = 1
 b = "2"
-
-print(int(2.5))
-print(str(a) + b)
+c = a + int(b)
+print("a =",a,"and it is",type(a))
+print("b =",b,"and it is",type(b))
+print("int(2.5) =",int(2.5))
+print("int(a) + int(b) =",int(a) + int(b))
 print(c)
-
-## Output
-# C:\Users\Rob\Desktop\AWS EC2\python-mega\Section18>py errors2.py
-# 2
-# 12
-# Traceback (most recent call last):
-#   File "errors2.py", line 5, in <module>
-#     print(c)
-# NameError: name 'c' is not defined
