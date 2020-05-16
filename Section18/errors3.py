b4518148c48a2fd4cfa19872d77ab6b343d2868c
Quacks <24840026+Quackers71@users.noreply.github.com>
2020-05-16 10:43:28 +0100

    Web searched the Error and possible Answer found

--- Section18/errors3.py
+++ Section18/errors3.py
@@ -11,3 +11,23 @@ print(c/0)
 #   File "errors3.py", line 5, in <module>
 #     print(c/0)
 # ZeroDivisionError: division by zero
+
+# So Web Search of "ZeroDivisionError: division by zero" found:
+# https://stackoverflow.com/questions/29836964/error-python-zerodivisionerror-division-by-zero
+# This shows:
+
+# Catch the error and handle it:
+
+# try:
+#     z = x / y
+# except ZeroDivisionError:
+#     z = 0
+# Or check before you do the division:
+
+# if y != 0:
+#     z = x / y
+# else:
+#     z = 0
+# The latter can be reduced to:
+
+# z = (x / y) if y != 0 else 0
