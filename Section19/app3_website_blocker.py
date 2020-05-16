41e6f94ede2962ab21a620ef1070760ba1dcd287
Quacks <24840026+Quackers71@users.noreply.github.com>
2020-05-16 21:04:33 +0100

    Setup the infinite loop

--- Section19/app3_website_blocker.py
+++ Section19/app3_website_blocker.py
@@ -1,10 +1,13 @@
 import time
+from datetime import datetime as dt
 
 hosts_path="C:\Windows\System32\drivers\etc\hosts"
 redirect="127.0.0.1"
 website_list=["www.facebook.com","facebook.com","www.purple.com","purple.com"]
 
 while True:
-
-    print(1)
-    time.sleep(5)
+    if dt(dt.now().year,dt.now().month,dt.now().day,16) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,23):
+        print("Working hours...")
+    else:
+        print("Fun hours...")
+        time.sleep(5)
