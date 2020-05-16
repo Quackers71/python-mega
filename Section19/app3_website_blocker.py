71ea10a2e1996ebe7d7abb3d293880d21960141a
Quacks <24840026+Quackers71@users.noreply.github.com>
2020-05-16 23:17:30 +0100

    added website list if it didn't exist

--- Section19/app3_website_blocker.py
+++ Section19/app3_website_blocker.py
@@ -1,14 +1,23 @@
 import time
 from datetime import datetime as dt
 
-hosts_path="hosts"
+hosts_temp="hosts"
+#hosts_path="C:\Windows\System32\drivers\etc\hosts"
 redirect="127.0.0.1"
 website_list=["www.facebook.com","facebook.com","www.purple.com","purple.com"]
 
 while True:
-    if dt(dt.now().year,dt.now().month,dt.now().day,19) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,23):
+    if dt(dt.now().year,dt.now().month,dt.now().day,16,1) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,23,59):
         print("Working hours...")
         time.sleep(5)
+        with open(hosts_temp,'r+') as file:
+            content=file.read()
+            print(content)
+            for website in website_list:
+                if website in content:
+                    pass
+                else:
+                    file.write(redirect+" "+website+"\n")
     else:
         print("Fun hours...")
         time.sleep(5)
