224e2c740318e0cd6120fb105f78e26c5226d4b6
Quacks <24840026+Quackers71@users.noreply.github.com>
2020-05-16 10:36:58 +0100

    errors3 Output

--- Section18/errors3.py
+++ Section18/errors3.py
@@ -3,3 +3,11 @@ b = "2"
 c = 3
 print(int(2.5))
 print(c/0)
+
+## Output
+# C:\Users\Rob\Desktop\AWS EC2\python-mega\Section18>py errors3.py
+# 2
+# Traceback (most recent call last):
+#   File "errors3.py", line 5, in <module>
+#     print(c/0)
+# ZeroDivisionError: division by zero
