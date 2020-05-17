75ab7e00fa40420153aaeabc46d7dba11e01a5dd
quackers71 <24840026+Quackers71@users.noreply.github.com>
2020-05-17 17:51:24 +0100

    app configured for working and non-working hours

--- Section19/app3_website_blocker.py
+++ Section19/app3_website_blocker.py
@@ -7,17 +7,23 @@ redirect="127.0.0.1"
 website_list=["www.facebook.com","facebook.com","www.purple.com","purple.com"]
 
 while True:
-    if dt(dt.now().year,dt.now().month,dt.now().day,16,1) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,23,59):
+    if dt(dt.now().year,dt.now().month,dt.now().day,23,1) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,23,59):
         print("Working hours...")
-        time.sleep(5)
         with open(hosts_temp,'r+') as file:
             content=file.read()
-            print(content)
+            # print(content)
             for website in website_list:
                 if website in content:
                     pass
                 else:
                     file.write(redirect+" "+website+"\n")
     else:
+        with open(hosts_temp, 'r+') as file:
+            content=file.readlines()
+            file.seek(0)
+            for line in content:
+                if not any(website in line for website in website_list):
+                    file.write(line)
+            file.truncate()
         print("Fun hours...")
-        time.sleep(5)
+    time.sleep(5)
