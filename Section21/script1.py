a5fd69842db8c2431b210fe1ade2dcd0399f8518
Quacks <24840026+Quackers71@users.noreply.github.com>
2020-05-21 21:34:09 +0100

    170. Setting up a GUI with Widgets

--- Section21/script1.py
+++ Section21/script1.py
@@ -1,2 +1,14 @@
 from tkinter import *
 
+window=Tk()
+
+b1=Button(window,text="Execute")
+b1.grid(row=0,column=0) # grid over pack as grid() has more control over Buttons or Widgets
+
+e1=Entry(window)
+e1.grid(row=0,column=1)
+
+t1=Text(window,height=1,width=20)
+t1.grid(row=0,column=2)
+
+window.mainloop()
